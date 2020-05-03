84186bb1741202b7cfc619add21646c41fd89edc
Matsumoto Miu <miu.matsumoto04@gmail.comgit config --global user.email miu.matsumoto04@gmail.com>
2020-05-04 08:38:49 +0900

    Updates

---
+++ precache-manifest.c1f44b9928cd9d80db9b8bd7672bc70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78652c37b6e5160b3f1766e7d25df567",
+    "url": "/food-searching-app/index.html"
+  },
+  {
+    "revision": "b8dac0e74ed37c782544",
+    "url": "/food-searching-app/static/css/main.61ef87a8.chunk.css"
+  },
+  {
+    "revision": "8d6aee74e62092fbdd9a",
+    "url": "/food-searching-app/static/js/2.3b0dc0d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-searching-app/static/js/2.3b0dc0d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8dac0e74ed37c782544",
+    "url": "/food-searching-app/static/js/main.497915f8.chunk.js"
+  },
+  {
+    "revision": "eb1b6896ed4930fb3400",
+    "url": "/food-searching-app/static/js/runtime-main.474a3b41.js"
+  }
+]);
